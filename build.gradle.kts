32bba7d48c2bbaf7e21ca1e6ee66fe798812bde1
leroy <leroytruong@gmail.com>
2020-04-02 22:07:41 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,25 +11,26 @@ group = "com.github.frozensync"
 version = "1.0-SNAPSHOT"
 
 val kotlinVersion by extra("1.3.70")
+val kotlinSerializationVersion by extra("0.20.0")
 
-val discord4j by extra("3.0.6")
-val kotlinLoggingVersion by extra("1.6.26")
+val discord4jVersion by extra("3.0.13")
+val kotlinLoggingVersion by extra("1.7.9")
 val logbackVersion by extra("1.2.3")
-val spekVersion by extra("2.0.2")
+val spekVersion by extra("2.0.10")
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$kotlinSerializationVersion")
 
-    implementation(group = "com.discord4j", name = "discord4j-core", version = discord4j)
-//    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.0.2.RELEASE")
+    implementation("com.discord4j:discord4j-core:$discord4jVersion")
 
-    implementation(group = "io.github.microutils", name = "kotlin-logging", version = kotlinLoggingVersion)
-    implementation(group = "ch.qos.logback", name = "logback-classic", version = logbackVersion)
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
 
     testImplementation(group = "org.jetbrains.kotlin", name = "kotlin-test", version = kotlinVersion)
 }
