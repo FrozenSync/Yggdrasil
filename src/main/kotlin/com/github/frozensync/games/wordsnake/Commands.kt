793ae4f40ee7da64316495138b5695c56774700c
leroy <leroytruong@gmail.com>
2020-05-14 04:35:50 +0200

    improvement(wordsnake): re-implement using an immutable design over event sourcing

--- src/main/kotlin/com/github/frozensync/games/wordsnake/Commands.kt
+++
@@ -1,5 +0,0 @@
-package com.github.frozensync.games.wordsnake
-
-internal data class CreateGameCommand(val channelId: Long, val players: List<Player>)
-internal data class AppendWordCommand(val channelId: Long, val player: Player, val word: String)
-internal data class UndoWordCommand(val channelId: Long, val player: Player)
--- src/main/kotlin/com/github/frozensync/games/wordsnake/WordSnakeCommandSet.kt
+++ src/main/kotlin/com/github/frozensync/games/wordsnake/WordSnakeCommandSet.kt
@@ -8,67 +8,56 @@ import kotlinx.coroutines.reactive.awaitFirst
 
 object WordSnakeCommandSet : CommandSet {
 
-    private const val GAME_STILL_ONGOING_MESSAGE = "There is already an on-going game in this channel"
-    private const val NO_ONGOING_GAME_MESSAGE =
-        "There is no on-going game in this channel. Please create one before playing."
+    private const val GAME_FOUND = "Found an on-going game in this channel. Please finish it before creating a new one."
+    private const val NO_GAME_FOUND = "No game found in this channel. Please create one before playing."
 
-    private val eventService: EventService = EventServiceImpl
-
-    private val wordSnakeRepository: WordSnakeRepository = InMemoryWordSnakeRepository
-    private val wordSnakeStatusRepository: WordSnakeStatusRepository = InMemoryWordSnakeStatusRepository
+    private val wordSnakeRepository: WordSnakeRepository = InMemoryWordSnakeRepository()
 
     override val commands: Map<String, Command> = mutableMapOf<String, Command>().apply {
         this["newgame"] = h@{ event ->
             val channel = event.message.channel.awaitFirst()
             val channelId = channel.id.asLong()
 
-            val conflict = wordSnakeRepository.findByChannel(channelId) != null
-            if (conflict) {
-                channel.createMessage(GAME_STILL_ONGOING_MESSAGE)
+            if (wordSnakeRepository.exists(channelId)) {
+                channel.createMessage(GAME_FOUND)
                 return@h
             }
 
-            val createGameCommand = event.message.userMentionIds
-                .map { Player(it.asLong()) }
-                .let { CreateGameCommand(channelId, players = it) }
+            val players = event.message.userMentionIds.map { Player(it.asLong()) }
+            val result = WordSnake(channelId, players)
+            // TODO validation
+            wordSnakeRepository.save(result)
 
-            val gameCreatedEvent = WordSnake.handle(createGameCommand)
-            if (gameCreatedEvent == null) { // TODO replace with Either<>
-                channel.createMessage("Cannot create a game with less than two players.").awaitFirst()
-                return@h
-            }
-            eventService.save(gameCreatedEvent)
-
-            val playerMentions = gameCreatedEvent.players.map { UserId(it.id).toString() }
+            val playerMentions = result.players.map { UserId(it.id).toString() }
             val message =
                 """Created a new game with the following players: $playerMentions
-                    |Turn 1: ${UserId(gameCreatedEvent.nextPlayer.id)}
+                    |Turn 1: ${UserId(result.currentPlayer.id)}
                 """.trimMargin()
             channel.createMessage(message).awaitFirst()
         }
 
         this["n"] = h@{ event ->
+            val player = event.message.author.map { Player(it.id.asLong()) }.orElse(null) ?: return@h
+            val word = event.message.content.map { CommandArgs(it).nextWord() }.orElse(null) ?: return@h
             val channel = event.message.channel.awaitFirst()
             val channelId = channel.id.asLong()
 
-            val game = wordSnakeRepository.findByChannel(channelId)
+            val game = wordSnakeRepository.findById(channelId)
             if (game == null) {
-                channel.createMessage(NO_ONGOING_GAME_MESSAGE).awaitFirst()
+                channel.createMessage(NO_GAME_FOUND).awaitFirst()
+                return@h
+            }
+            if (player != game.currentPlayer) {
+                channel.createMessage("It's not your turn ${UserId(player.id)}!").awaitFirst()
                 return@h
             }
 
-            val player = event.message.author.map { Player(it.id.asLong()) }.orElse(null) ?: return@h
-            val appendWordCommand = event.message.content
-                .map { AppendWordCommand(channelId, player, CommandArgs(it).nextWord()) }
-                .orElse(null) ?: return@h
-
             val message = try { // TODO replace with Either<>
-                val wordAppendedEvent = game.handle(appendWordCommand)
-                eventService.save(wordAppendedEvent)
+                val result = game.appendWord(word)
+                wordSnakeRepository.save(result)
 
-                val status = wordSnakeStatusRepository.findByChannel(channelId)
-                """Word: ${wordAppendedEvent.word}
-                    |Turn ${status?.turn}: ${UserId(wordAppendedEvent.nextPlayer.id)}
+                """Word: ${result.currentWord}
+                    |Turn ${result.turn}: ${UserId(result.currentPlayer.id)}
                 """.trimMargin()
             } catch (e: InvalidWordException) {
                 e.message!!
@@ -76,41 +65,16 @@ object WordSnakeCommandSet : CommandSet {
             channel.createMessage(message).awaitFirst()
         }
 
-        this["undo"] = h@{ event ->
-            val channel = event.message.channel.awaitFirst()
-            val channelId = channel.id.asLong()
-
-            val game = wordSnakeRepository.findByChannel(channelId)
-            if (game == null) {
-                channel.createMessage(NO_ONGOING_GAME_MESSAGE).awaitFirst()
-                return@h
-            }
-
-            val player = event.message.author.map { Player(it.id.asLong()) }.orElse(null) ?: return@h
-            val undoWordCommand = UndoWordCommand(channelId, player)
-            val wordUndoneEvent = game.handle(undoWordCommand) ?: return@h
-            eventService.save(wordUndoneEvent)
-
-            val status = wordSnakeStatusRepository.findByChannel(channelId)
-            val message =
-                """Undone ${wordUndoneEvent.removedWord}
-                    |Word: ${wordUndoneEvent.currentWord ?: ""}
-                    |Turn ${status?.turn}: ${UserId(wordUndoneEvent.nextPlayer.id)}
-                """.trimMargin()
-            channel.createMessage(message).awaitFirst()
-        }
-
         this["currentturn"] = { event ->
             val channel = event.message.channel.awaitFirst()
             val channelId = channel.id.asLong()
 
-            val message = wordSnakeStatusRepository.findByChannel(channelId)
-                ?.let { status ->
-                    """Word: ${status.lastWord}
-                        |Turn ${status.turn}: ${UserId(status.currentPlayer.id)}
-                    """.trimMargin()
-                }
-                ?: NO_ONGOING_GAME_MESSAGE
+            val message = when (val game = wordSnakeRepository.findById(channelId)) {
+                null -> NO_GAME_FOUND
+                else -> """Word: ${game.currentWord}
+                            |Turn ${game.turn}: ${UserId(game.currentPlayer.id)}
+                        """.trimMargin()
+            }
             channel.createMessage(message).awaitFirst()
         }
 
@@ -118,13 +82,15 @@ object WordSnakeCommandSet : CommandSet {
             val channel = event.message.channel.awaitFirst()
             val channelId = channel.id.asLong()
 
-            val message = wordSnakeStatusRepository.findByChannel(channelId)
-                ?.let { status ->
-                    """Size: ${status.numberOfCharacters}
-                        |Number of words: ${status.turn - 1}
+            val message = when (val game = wordSnakeRepository.findById(channelId)) {
+                null -> NO_GAME_FOUND
+                else -> {
+                    val statistics = game.getStatistics()
+                    """Length: ${statistics.snakeLength}
+                        |Number of words: ${statistics.numberOfWords}
                     """.trimMargin()
                 }
-                ?: NO_ONGOING_GAME_MESSAGE
+            }
             channel.createMessage(message).awaitFirst()
         }
     }
