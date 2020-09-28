e41aa9a21831618b4bff9743fd793c1436ba5077
leroy <leroytruong@gmail.com>
2020-09-29 01:06:26 +0200

    improvement: automatically disconnect the bot if it's alone in a voice channel

--- src/main/kotlin/com/github/frozensync/music/MusicCommandSet.kt
+++ src/main/kotlin/com/github/frozensync/music/MusicCommandSet.kt
@@ -3,7 +3,11 @@ package com.github.frozensync.music
 import com.github.frozensync.discord.command.Command
 import com.github.frozensync.discord.command.CommandArgs
 import com.github.frozensync.discord.command.CommandSet
+import discord4j.core.`object`.VoiceState
+import discord4j.core.event.domain.VoiceStateUpdateEvent
 import kotlinx.coroutines.reactive.awaitFirstOrNull
+import reactor.core.publisher.Mono
+import java.time.Duration
 
 class MusicCommandSet : CommandSet {
 
@@ -15,7 +19,30 @@ class MusicCommandSet : CommandSet {
 
             val provider = GuildAudioManager.of(channel.guildId).provider
 
-            channel.join { spec -> spec.setProvider(provider) }.block()
+            channel
+                .join { spec -> spec.setProvider(provider) }
+                .flatMap { connection ->
+                    val isAlone = channel.voiceStates
+                        .count()
+                        .map { count -> 1L == count }
+
+                    val onDelay = Mono.delay(Duration.ofSeconds(10L))
+                        .filterWhen { isAlone }
+                        .switchIfEmpty(Mono.never())
+                        .then()
+
+                    val onEvent = channel.client.eventDispatcher.on(VoiceStateUpdateEvent::class.java)
+                        // filter is not strictly necessary, but it does prevent many unnecessary cache calls
+                        .filter { event ->
+                            event.old.flatMap(VoiceState::getChannelId).map(channel.id::equals).orElse(false)
+                        }
+                        .filterWhen { isAlone }
+                        .next()
+                        .then()
+
+                    Mono.first(onDelay, onEvent).then(connection.disconnect())
+                }
+                .block()
         }
 
         this["play"] = h@{ event ->
